c10dcf389c8d8ce6ec1774c616158d70583c91a1
kans-th0 <kans-th0@klth144945.net.lth.se>
2014-11-10 13:53:48 +0100

    New Commit

--- res/tx_extend_feeditadvanced.js
+++ res/tx_extend_feeditadvanced.js
@@ -452,11 +452,12 @@ function editPage()
     //toggleItem('.feEditAdvanced-pageItemsMenu');
 }
 
-function newPage()
+function newPage(below)
 {
     showAjaxLoadingIcon();
     var pid = $('#pageid').val();
-    var url = 'index.php?eID=feeditadvanced&TSFE_EDIT%5Brecord%5D=pages%3A'+pid+'&TSFE_EDIT%5Bpid%5D='+pid+'&TSFE_EDIT%5BnewRecordInPid%5D='+pid+'&TSFE_EDIT[cmd]=new&pid='+pid+'&lang='+$('#beuser_lang').val();
+    var url = 'index.php?eID=feeditadvanced&TSFE_EDIT%5Brecord%5D=pages%3A'+below+pid+'&TSFE_EDIT%5Bpid%5D='+pid+'&TSFE_EDIT%5BnewRecordInPid%5D='+pid+'&TSFE_EDIT[cmd]=new&pid='+pid+'&lang='+$('#beuser_lang').val();
+    //http://typotest-2.kansli.lth.se/typo3/alt_doc.php?returnUrl=%2Ftypo3%2Fsysext%2Fcms%2Flayout%2Fdb_layout.php%3Fid%3D5&edit[pages][-5]=new&returnNewPageId=1
     var headerText = resources.newPageHeader+'<span onclick="closeIframe();return false;" class="fancybox-close"></span><span onclick="loadHelp();return false;" class="help-button">';
     height = TYPO3.configuration.feeditadvanced.editWindow.height ? parseInt(TYPO3.configuration.feeditadvanced.editWindow.height) : 600;
     width = TYPO3.configuration.feeditadvanced.editWindow.width ? parseInt(TYPO3.configuration.feeditadvanced.editWindow.width) : 800;
