c888d9db54ce79ba876414e8618160e96dffc9c0
kans-th0 <kans-th0@klth144945.net.lth.se>
2014-12-01 15:54:50 +0100

    New commit

--- ext_tables.php
+++ ext_tables.php
@@ -3,8 +3,9 @@ if (!defined ('TYPO3_MODE')) 	die ('Access denied.');
 
 t3lib_extMgm::allowTableOnStandardPages('pages');
 
-$GLOBALS['TBE_MODULES_EXT']['xMOD_db_new_content_el']['addElClasses']['tx_mailformplusadmin_pi2_wizicon'] = t3lib_extMgm::extPath('mailformplus_admin') . 'pi2/class.tx_mailformplusadmin_pi2_wizicon.php';
-
+if (t3lib_extMgm::isLoaded('mailformplus_admin')) {
+    $GLOBALS['TBE_MODULES_EXT']['xMOD_db_new_content_el']['addElClasses']['tx_mailformplusadmin_pi2_wizicon'] = t3lib_extMgm::extPath('mailformplus_admin') . 'pi2/class.tx_mailformplusadmin_pi2_wizicon.php';
+}
 /*t3lib_extMgm::addPageTSConfig('
 	mod.extendfeadv {
 	list {
